dc0bed35dd18c1db6333aa8b4febe5b17c2d6525
Louis Blonde <loul0uboypy@gmail.com>
2024-12-03 16:00:39 +0100

    add verif 4

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 def grille_vide():
-    return [[0 for i in range(7)] for j in range(6)]
+    return [[1 for i in range(7)] for j in range(6)]
 def affiche(g):
     for i in range(len(g)):
         for j in range(len(g[i])):
@@ -29,4 +29,18 @@ def jouer(g, j, c):
                 g[i - 1][c] = point
                 break
             i -= 1
-    return g
+def horiz(g, j, l, c):
+    for i in range(l, l + 4):
+        if g[i][c] != j:
+            return False
+    return True
+def vert(g, j, l, c):
+    for i in range(c, c + 4):
+        if g[l][i] != j:
+            return False
+    return True
+def diag(g, j, l, c):
+    for i in range(4):
+        if g[l + i][c + i] != j:
+            return False
+    return True
